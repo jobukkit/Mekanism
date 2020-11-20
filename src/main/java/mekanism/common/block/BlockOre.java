16b1cbc81fb7a65543d83ef8b625deaf4522c85b
Richard Freimer <richard@freimer.com>
2020-11-19 19:27:53 -0500

    Fix oversight of not having fluorite ore drop any xp

--- src/main/java/mekanism/common/block/BlockOre.java
+++ src/main/java/mekanism/common/block/BlockOre.java
@@ -4,7 +4,11 @@ import mekanism.api.text.ILangEntry;
 import mekanism.common.block.interfaces.IHasDescription;
 import mekanism.common.resource.OreType;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nonnull;
@@ -22,5 +26,13 @@ public class BlockOre extends Block implements IHasDescription {
     @Override
     public ILangEntry getDescription() {
         return () -> "description.mekanism." + ore.getResource().getRegistrySuffix() + "_ore";
+    }
+
+    @Override
+    public int getExpDrop(BlockState state, IWorldReader reader, BlockPos pos, int fortune, int silkTouch) {
+        if (ore.getMaxExp() > 0 && silkTouch == 0) {
+            return MathHelper.nextInt(RANDOM, ore.getMinExp(), ore.getMaxExp());
+        }
+        return super.getExpDrop(state, reader, pos, fortune, silkTouch);
     }
 }
--- src/main/java/mekanism/common/resource/OreType.java
+++ src/main/java/mekanism/common/resource/OreType.java
@@ -5,7 +5,7 @@ public enum OreType {
     TIN(PrimaryResource.TIN, 14, 8, 0, 0, 60),
     OSMIUM(PrimaryResource.OSMIUM, 12, 8, 0, 0, 60),
     URANIUM(PrimaryResource.URANIUM, 8, 8, 0, 0, 60),
-    FLUORITE(MiscResource.FLUORITE, 6, 12, 0, 0, 32),
+    FLUORITE(MiscResource.FLUORITE, 6, 12, 0, 0, 32, 1, 4),
     LEAD(PrimaryResource.LEAD, 8, 8, 0, 0, 48);
 
     private final IResource resource;
@@ -14,14 +14,22 @@ public enum OreType {
     private final int bottomOffset;
     private final int topOffset;
     private final int maxHeight;
+    private final int minExp;
+    private final int maxExp;
 
     OreType(IResource resource, int perChunk, int maxVeinSize, int bottomOffset, int topOffset, int maxHeight) {
+        this(resource, perChunk, maxVeinSize, bottomOffset, topOffset, maxHeight, 0, 0);
+    }
+
+    OreType(IResource resource, int perChunk, int maxVeinSize, int bottomOffset, int topOffset, int maxHeight, int minExp, int maxExp) {
         this.resource = resource;
         this.perChunk = perChunk;
         this.maxVeinSize = maxVeinSize;
         this.bottomOffset = bottomOffset;
         this.topOffset = topOffset;
         this.maxHeight = maxHeight;
+        this.minExp = minExp;
+        this.maxExp = maxExp;
     }
 
     public IResource getResource() {
@@ -46,6 +54,14 @@ public enum OreType {
 
     public int getMaxHeight() {
         return maxHeight;
+    }
+
+    public int getMinExp() {
+        return minExp;
+    }
+
+    public int getMaxExp() {
+        return maxExp;
     }
 
     public static OreType get(IResource resource) {
